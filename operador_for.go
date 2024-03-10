46990d4ed4a2b41493700f9baf1ede9847d543f7
Fabiano Amaral <fabyano.amaral@gmail.com>
2024-03-10 16:02:52 -0400

    Imprimir números de 1 a 100. No lugar de número multiplo de 3 imprimir 'Pin', no lugar de múltiplo de 5 imprimir 'Pan',e caso seja divisivel por ambos, imprimir 'PinPan'

--- operador_for.go
+++ operador_for.go
@@ -9,9 +9,28 @@ func main() {
 			fmt.Println(i)
 		}
 	}
+
+	fmt.Println("Número múltiplo de 3 = 'Pin'")
+	fmt.Println("Número múltiplo de 5 = 'Pan'")
+	fmt.Println("Número múltiplo de 3 e 5 = 'PinPan'")
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("PinPan")
+		} else if i%3 == 0 {
+			fmt.Println("Pin")
+		} else if i%5 == 0 {
+			fmt.Println("Pan")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
 
 // Resolvendo Problemas Numéricos com Go - parte 01
-/*
-Criar um código que exiba todos os números entre 1 e 100 que são divisíveis por 3.
-*/
+/*Criar um código que exiba todos os números entre 1 e 100 que são divisíveis por 3.*/
+
+// Resolvendo Problemas Numéricos com Go - parte 02
+/*Criar um código que deve imprimir números de 1 a 100.
+No lugar de número multiplo de 3 imprimir "Pin",
+no lugar de múltiplo de 5 imprimir "Pan",
+e caso seja divisivel por ambos, imprimir "PinPan".*/
